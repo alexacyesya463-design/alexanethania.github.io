1e4e2b36ed6fbe7557819653e3c0b1ea68d43a78
alexacyeysa463 <alexacyesya463@gmail.com>
2026-03-01 22:05:59 +0700

    barulg

--- biodata pengenalaan/script.js
+++ biodata pengenalaan/script.js
@@ -1,15 +1,20 @@
-// Dark Mode Toggle
-const darkBtn = document.getElementById("darkModeBtn");
-darkBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+// Mobile Menu
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
 });
 
-// Scroll to Top Button
-const scrollBtn = document.getElementById("scrollTopBtn");
-
-scrollBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+// Scroll Button Function
+function scrollToSection(id){
+    document.getElementById(id).scrollIntoView({
+        behavior:"smooth"
     });
+}
+
+// Form Alert
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Pesan berhasil dikirim ✨");
 });
