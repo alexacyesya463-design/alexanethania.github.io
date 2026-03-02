fe81ae158ff1c381eb59e24730442f30e423bf85
alexacyeysa463 <alexacyesya463@gmail.com>
2026-03-02 13:11:16 +0700

    o

--- biodata pengenalaan/script.js
+++ biodata pengenalaan/script.js
@@ -1,20 +1,12 @@
-// Mobile Menu
-const toggle = document.getElementById("menu-toggle");
-const navLinks = document.querySelector(".nav-links");
+const toggle=document.getElementById("menu-toggle");
+const navLinks=document.querySelector(".nav-links");
 
-toggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+toggle.addEventListener("click",()=>{
+navLinks.classList.toggle("active");
 });
 
-// Scroll Button Function
 function scrollToSection(id){
-    document.getElementById(id).scrollIntoView({
-        behavior:"smooth"
-    });
-}
-
-// Form Alert
-document.getElementById("contactForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Pesan berhasil dikirim ✨");
+document.getElementById(id).scrollIntoView({
+behavior:"smooth"
 });
+}
