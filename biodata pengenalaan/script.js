ec1111e355fbdf9d3685c8e547f64dc4c72a99c7
alexacyeysa463 <alexacyesya463@gmail.com>
2026-03-10 14:09:17 +0700

    .

--- biodata pengenalaan/script.js
+++ biodata pengenalaan/script.js
@@ -1,7 +1,7 @@
-const toggle=document.getElementById("menu-toggle");
-const navLinks=document.querySelector(".nav-links");
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-toggle.addEventListener("click",()=>{
+toggle.addEventListener("click", () => {
 navLinks.classList.toggle("active");
 });
 
@@ -10,9 +10,11 @@ document.getElementById(id).scrollIntoView({
 behavior:"smooth"
 });
 }
+
 const audio = document.getElementById("myAudio");
 const playBtn = document.getElementById("playBtn");
 const progressBar = document.getElementById("progressBar");
+const time = document.getElementById("time");
 
 function togglePlay() {
     if (audio.paused) {
@@ -24,36 +26,37 @@ function togglePlay() {
     }
 }
 
-function rewind() { audio.currentTime -= 10; }
-function forward() { audio.currentTime += 10; }
+function rewind() { 
+audio.currentTime -= 10; 
+}
+
+function forward() { 
+audio.currentTime += 10; 
+}
 
 audio.ontimeupdate = function() {
+
     if (audio.duration) {
+
         const progress = (audio.currentTime / audio.duration) * 100;
         progressBar.value = progress;
+
+        let current = audio.currentTime;
+        let duration = audio.duration;
+
+        let menit = Math.floor(current / 60);
+        let detik = Math.floor(current % 60);
+
+        let menitTotal = Math.floor(duration / 60);
+        let detikTotal = Math.floor(duration % 60);
+
+        if(detik < 10) detik = "0" + detik;
+        if(detikTotal < 10) detikTotal = "0" + detikTotal;
+
+        time.innerHTML = menit + ":" + detik + " / " + menitTotal + ":" + detikTotal;
     }
 };
 
 function changeProgress() {
-    audio.currentTime = (progressBar.value / 100) * audio.duration;
+audio.currentTime = (progressBar.value / 100) * audio.duration;
 }
-let music = document.getElementById("musik");
-let time = document.getElementById("time");
-
-music.addEventListener("timeupdate", function(){
-
-let current = music.currentTime;
-let duration = music.duration;
-
-let menit = Math.floor(current / 60);
-let detik = Math.floor(current % 60);
-
-let menitTotal = Math.floor(duration / 60);
-let detikTotal = Math.floor(duration % 60);
-
-if(detik < 10) detik = "0" + detik;
-if(detikTotal < 10) detikTotal = "0" + detikTotal;
-
-time.innerHTML = menit + ":" + detik + " / " + menitTotal + ":" + detikTotal;
-
-});
